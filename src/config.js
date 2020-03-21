a92107851cfe1c369d73c64abef993d5d5e29e74
6035512023 <6035512023@psu.ac.th>
2020-03-22 00:17:10 +0700

    commit5

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyDFYm61j-cCPOW8ZsDch5dzStt00FNLgAg",
+  authDomain: "todo-cd5b8.firebaseapp.com",
+  databaseURL: "https://todo-cd5b8.firebaseio.com",
+  projectId: "todo-cd5b8",
+  storageBucket: "todo-cd5b8.appspot.com",
+  messagingSenderId: "971451796847",
+  appId: "1:971451796847:web:ade67cb49e7383455a2c9a",
+  measurementId: "G-0NBPMW5YY7"
+  };
+export default config; 
