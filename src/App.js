25dce64d5cd1dd6b3c1dc5a81089cb5961742ac8
6035512023 <6035512023@psu.ac.th>
2020-03-22 00:53:03 +0700

    commit10

--- src/App.js
+++ src/App.js
@@ -2,11 +2,8 @@ import React, { useState,useEffect } from 'react';
 import './App.css';
 import {firestore} from './index'
 function App() {
-  
   const [tasks,setTasks] = useState([])
-
   const [ name,setName ] = useState([
-
   ])
   useEffect( () => {
     retriveData()
@@ -22,31 +19,32 @@ function App() {
       setTasks(myTask)
     } )
   }
-
   const deleteTask = (id) => {
     firestore.collection("tasks").doc(id+'').delete()
+  }
+
+  const editTask = (id) => {
+    firestore.collection("tasks").doc(id+'').set({id,name})
   }
 
   const renderTask = () => {
     if (tasks && tasks.length)
       return tasks.map((task,index)=>{
           return(
-          
             <li key={index}> 
             {task.id} : {task.name}
             <button onClick={() => deleteTask(task.id)}>Delete</button>
+            <button onClick={() => editTask(task.id)}>Edit</button>
             </li>
           )
         })
     else
         return (<li>No task</li>)
   }
-
   const addTask = () => {
     let id = ( tasks.length ===0)?1:tasks[tasks.length-1].id+1
     firestore.collection("tasks").doc(id+'').set({id,name})
   }
-
   return (
     <div >
         <h1>Todo</h1>
